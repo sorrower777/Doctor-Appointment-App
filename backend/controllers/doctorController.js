4d5ad311c5f38f46dce371effe1731703e632ec5
Ritom <chakrabortyarindam053@gmail.com>
2025-08-16 23:38:08 +0530

    feat: Implement doctor login functionality with JWT authentication

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -1,6 +1,7 @@
 
-
+import bcrypt from "bcrypt";
 import doctorModel from "../models/doctorModel.js";
+import jwt from "jsonwebtoken";
 
 const changeAvailability = async (req, res) => {
     try {
@@ -26,4 +27,30 @@ const doctorList = async (req, res) => {
     }
 }
 
-export { changeAvailability, doctorList };
+//API for doctor login
+const loginDoctor = async (req, res) => {
+    try{
+        const { email, password} = req.body;
+        const doctor = await doctorModel.findOne({email})
+
+        if(!doctor){
+            return res.json({success: false, message: 'Invalid credentials'})
+        }
+        const isMatch = await bcrypt.compare(password, doctor.password);
+
+        if(isMatch) {
+            const token = jwt.sign({id:doctor._id}, process.env.JWT_SECRET)
+            res.json({success: true, token})
+        }
+        else {
+            res.json({success: false, message: error.message})
+        }
+
+    }
+    catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message});
+    }
+}
+
+export { changeAvailability, doctorList, loginDoctor };
